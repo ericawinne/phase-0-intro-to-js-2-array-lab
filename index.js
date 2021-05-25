deac63539c737334646bf1a245220c75c1642734
Erica Winne <erica.winne@powerhrg.com>
2021-05-24 21:14:21 -0400

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+} 
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const newCat = [...cats, name]
+    return newCat
+}
+function prependCat(name) {
+    const newCat = [name, ...cats]
+    return newCat
+}
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
